dbb5bf4fcb5989c2a3dbdd17cc99e3ac92642624
Alejandro202010329 <202010329@itla.edu.do>
2022-11-26 21:40:40 -0400

    HarperDB-Implementation

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from database import db
+
 app = FastAPI()
 
 @app.get('/')
 def getRoutes():
     return ['/notes', '/notes/<pk>']
+
+@app.get('/notes')
+def getNotes():
+   #notes = db.search_by_value('notesapp', 'notes', "id", "*", get_attributes=['*'])
+    notes = db.sql('SELECT * FROM notesapp.notes ORDER BY __updatetime__ DESC')
+    return notes
+
+@app.get("/notes/{pk}")
+def getNote(pk:str):
+    notes = db.search_by_hash('notesapp', 'notes', [pk], get_attributes=['*'])
+    return notes[0]
+
+@app.post("/notes")
+def addNotes(data = Body()):
+    db.insert('notesapp', 'notes', [{"body":data['body']}])
+    notes = db.search_by_value('notesapp', 'notes', "id", "*", get_attributes=['*'])
+    return notes
+
+
+
+@app.put("/notes/{id}")
+def updateNote(id:str, data = Body()):
+    db.update('notesapp', 'notes', [{"id":id, "body":data["body"]}])
+    notes = db.search_by_value('notesapp', 'notes', "id", "*", get_attributes=['*'])
+    return notes
+
+@app.delete("/notes/{id}")
+def deleteNote(id:str):
+    db.delete('notesapp', 'notes', [id])
+    notes = db.search_by_value('notesapp', 'notes', "id", "*", get_attributes=['*'])
+    return notes
---
+++ secrets.py
@@ -0,0 +1,3 @@
+HARPERDB_PASSWORD = 'aL3*183023'
+HARPERDB_URL = "https://cloud-1-notesapp.harperdbcloud.com"
+HARPERDB_USERNAME = "alex"
